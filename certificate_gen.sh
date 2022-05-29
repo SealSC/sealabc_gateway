43952eccab41d8ac05526a73fa4fe175cb905dfe
d5c5ceb0 <d5c5ceb0@gmail.com>
2022-05-29 20:28:21 +0800

    update certificate generator script

--- certificate_gen.sh
+++ certificate_gen.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-openssl genrsa -aes256 -out server.key 2048
-openssl rsa -in server.key -out server.key
-openssl req -new -key server.key -out server.csr
+# v1
+#openssl genrsa -aes256 -out server.key 2048
+#openssl rsa -in server.key -out server.key
+#openssl req -new -key server.key -out server.csr
+#openssl req -new -x509 -key server.key -out ca.crt -days 3650
+#openssl x509 -req -days 3650 -in server.csr -CA ca.crt -CAkey server.key -CAcreateserial -out server.crt
 
-openssl req -new -x509 -key server.key -out ca.crt -days 3650
-
-openssl x509 -req -days 3650 -in server.csr -CA ca.crt -CAkey server.key -CAcreateserial -out server.crt
+#v2
+sudo openssl req \
+    -x509 \
+    -nodes \
+    -days 365 \
+    -newkey rsa:2048 \
+    -keyout server.key \
+    -out server.crt
