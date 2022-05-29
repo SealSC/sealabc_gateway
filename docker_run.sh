b8d67bab341d8c0c88f569e03b1e70cd2b52321d
d5c5ceb0 <d5c5ceb0@gmail.com>
2022-05-29 18:01:51 +0800

    add docker script

---
+++ docker_run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir -p logs
+
+sudo docker run \
+    --name sealabc_gateway \
+    -v $PWD/nginx.conf:/usr/local/openresty/nginx/conf/nginx.conf \
+    -v $PWD/conf.d:/usr/local/openresty/nginx/conf/conf.d \
+    -v $PWD/logs:/var/log/nginx \
+    --net="host" \
+    --rm \
+    openresty/openresty:alpine
+
+    #-v $PWD/conf.d:/etc/nginx/conf.d \
